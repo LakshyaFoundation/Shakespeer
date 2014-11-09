b9f14321bcc3242cf5c3a9c4a547f7b32935c332
Sarath Somana <srthsomana@gmail.com>
2014-11-09 11:21:01 +0530

    Added requirements.sh

---
+++ requirements.sh
@@ -0,0 +1,8 @@
+pip install --upgrade Django==1.6.6
+pip install --upgrade South==1.0
+pip install --upgrade argparse==1.2.1
+pip install --upgrade wsgiref==0.1.2
+pip install --upgrade mysql-python
+pip install --upgrade python-social-auth
+pip install --upgrade gunicorn
+easy_install PIL
